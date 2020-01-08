a57089c3ed96fe5725fc3150239929850369d974
kieranprice <41809477+kieranprice@users.noreply.github.com>
2020-01-08 18:22:06 +0000

    Created monolog channel

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ ini_set('xdebug.trace_output_name', 'calculate_example.%t');
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 require '../vendor/autoload.php';
 
@@ -17,6 +19,11 @@ $container = new \Slim\Container($settings);
 require $app_dir . 'dependencies.php';
 
 $app = new \Slim\App($container);
+
+// create a monolog channel
+
+$log = new Logger('logger');
+$log->pushHandler(new StreamHandler(dirname(__FILE__) . '/log.txt', Logger::WARNING));
 
 require $app_dir . 'routes.php';
 
