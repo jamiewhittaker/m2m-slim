94e9d4e69d7ce3895933cdd2fbed69c85952c89e
Christian Machado Lopes <46377202+ChrisFlopes@users.noreply.github.com>
2020-01-09 21:51:10 +0000

    Passing dummy data through

--- app/routes/status.php
+++ app/routes/status.php
@@ -3,7 +3,20 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->get('/status', function(Request $request, Response $response)
-{
-    return $this->view->render($response, 'status.html.twig');
-})->setName('status');
+$app->get(
+    '/status',
+    function(Request $request, Response $response) use ($app)
+    {
+        return $this->view->render($response,
+            'status.html.twig',
+            [
+                'fan_direction' => 'reverse',
+                'temperature' => '65°C',
+                'keypad_number' => '5',
+                'switches' => ['On', 'Off', 'Off', 'On'],
+                'sim_number' => '01234 567890',
+                'name' => 'Micheal Keaton',
+                'email' => 'mkeaton@hollywood.com'
+            ]);
+
+    });
