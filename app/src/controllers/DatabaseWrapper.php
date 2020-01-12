33a85686e2024b62c816c38653f52d0438dfcd7e
Jamie Whittaker <33399796+jamiewhittaker@users.noreply.github.com>
2020-01-12 17:22:03 +0000

    Database only updates when new message received

--- app/src/controllers/DatabaseWrapper.php
+++ app/src/controllers/DatabaseWrapper.php
@@ -10,7 +10,7 @@ use DateTime;
 use Exception;
 use PDO;
 use App\Models\CircuitStatus;
-
+use App\Controllers\MetadataValidator;
 
 
 class DatabaseWrapper
@@ -19,65 +19,68 @@ class DatabaseWrapper
 
     public function insertBoardStatus($status)
     {
+        $val = new MetadataValidator();
+        if (!$val->checkDateRecieved($status)) {
 
-        try {
+            try {
 
-            /**
-             * Create a new PDO object connecting to the database
-             */
-            $this->database = new PDO('mysql:host=localhost;dbname=m2m_slim', 'm2mslim', 'DMUcoursework1');
-            $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                /**
+                 * Create a new PDO object connecting to the database
+                 */
+                $this->database = new PDO('mysql:host=localhost;dbname=m2m_slim', 'm2mslim', 'DMUcoursework1');
+                $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            /**
-             * Getting the metadata and circuit board status using get methods from the CircuitStatus class
-             * to store in variables.
-             */
-            $date = date("Y-m-d H:i:s");
-            $name = $status->getName();
-            $email = $status->getEmail();
-            $msisdn = $status->getMsisdn();
-            $s1 = $status->getSwitch1();
-            $s2 = $status->getSwitch2();
-            $s3 = $status->getSwitch3();
-            $s4 = $status->getSwitch4();
-            $fan = $status->getFan();
-            $temp = $status->getTemp();
-            $keypad = $status->getKeypad();
+                /**
+                 * Getting the metadata and circuit board status using get methods from the CircuitStatus class
+                 * to store in variables.
+                 */
+                $date = $status->getDate();
+                $name = $status->getName();
+                $email = $status->getEmail();
+                $msisdn = $status->getMsisdn();
+                $s1 = $status->getSwitch1();
+                $s2 = $status->getSwitch2();
+                $s3 = $status->getSwitch3();
+                $s4 = $status->getSwitch4();
+                $fan = $status->getFan();
+                $temp = $status->getTemp();
+                $keypad = $status->getKeypad();
 
 
-            /**
-             * Preparing sql statement which will be used to insert data to the board_status table
-             */
-            $sql = $this->database->prepare("INSERT INTO `board_status` (date, name, email, msisdn, switch1, switch2, switch3, switch4, fan, temp, keypad)
+                /**
+                 * Preparing sql statement which will be used to insert data to the board_status table
+                 */
+                $sql = $this->database->prepare("INSERT INTO `board_status` (date, name, email, msisdn, switch1, switch2, switch3, switch4, fan, temp, keypad)
             VALUES (:date, :name , :email, :msisdn, :switch1, :switch2, :switch3, :switch4, :fan, :temp, :keypad)");
 
-            /**
-             * Binding parameters so that they can be used in the sql statement which was prepared
-             */
+                /**
+                 * Binding parameters so that they can be used in the sql statement which was prepared
+                 */
 
-            $sql->bindParam(':date', $date);
-            $sql->bindParam(':email', $email);
-            $sql->bindParam(':name', $name);
-            $sql->bindParam(':msisdn', $msisdn);
-            $sql->bindParam(':switch1', $s1);
-            $sql->bindParam(':switch2', $s2);
-            $sql->bindParam(':switch3', $s3);
-            $sql->bindParam(':switch4', $s4);
-            $sql->bindParam(':fan', $fan);
-            $sql->bindParam(':temp', $temp);
-            $sql->bindParam(':keypad', $keypad);
+                $sql->bindParam(':date', $date);
+                $sql->bindParam(':email', $email);
+                $sql->bindParam(':name', $name);
+                $sql->bindParam(':msisdn', $msisdn);
+                $sql->bindParam(':switch1', $s1);
+                $sql->bindParam(':switch2', $s2);
+                $sql->bindParam(':switch3', $s3);
+                $sql->bindParam(':switch4', $s4);
+                $sql->bindParam(':fan', $fan);
+                $sql->bindParam(':temp', $temp);
+                $sql->bindParam(':keypad', $keypad);
 
-            /**
-             * If execute() returns false a new exception will be thrown
-             */
-            if (
-                $sql->execute() === false) {
-                throw new exception("Unable to add to database");
+                /**
+                 * If execute() returns false a new exception will be thrown
+                 */
+                if (
+                    $sql->execute() === false) {
+                    throw new exception("Unable to add to database");
+                }
+
+            } catch (\PDOException $e) {
+                echo "Error: " . $e->getMessage();
             }
-
-        } catch (\PDOException $e) {
-            echo "Error: " . $e->getMessage();
         }
     }
-    
+
 }
---
+++ app/src/controllers/MetadataValidator.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Checks metadata stored in a row to avoid duplication of data
+ */
+
+
+namespace App\controllers;
+
+
+use PDO;
+
+class MetadataValidator
+{
+    private $database;
+
+    public function checkDateRecieved($status) {
+
+
+        try {
+
+            /**
+             * Create a new PDO object connecting to the database
+             */
+            $this->database = new PDO('mysql:host=localhost;dbname=m2m_slim', 'm2mslim', 'DMUcoursework1');
+            $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            /**
+             * Getting the metadata and circuit board status using get methods from the CircuitStatus class
+             * to store in variables.
+             */
+            $date = $status->getDate();
+
+            /**
+             * Preparing sql statement which will be used to count the number of rows with a certain date
+             */
+            $sql = $this->database->prepare("SELECT COUNT(*) FROM `board_status` WHERE date = :date");
+
+            /**
+             * Binding parameters so that they can be used in the sql statement which was prepared
+             */
+
+            $sql->bindParam(':date', $date);
+
+            /**
+             * If execute() returns false a new exception will be thrown
+             */
+            if (
+                $sql->execute() === false) {
+                throw new exception("Unable to add to database");
+            }
+
+            $count = $sql->fetchColumn();
+
+            if ($count > 0) {
+                return true;
+            }
+
+            if ($count == 0) {
+                return false;
+            }
+
+        } catch (\PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+
+
+}
